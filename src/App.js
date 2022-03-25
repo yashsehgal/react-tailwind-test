f47f8b8befa7759f1224ef40a0fbf07787183c83
Yash Sehgal <yashsehgal.work@gmail.com>
2022-03-25 19:50:22 +0000

    code: github-api widget maker added (wip)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 export default function App() {
   const [widgetState, setWidget] = useState(false);
+  const [githubUserDataRef, setGithubUserData] = useState([]);
   return (
     <div className="app m-8">
       <h4 className="text-sm uppercase font-semibold text-indigo-600">Github API</h4>
@@ -20,18 +21,23 @@ export default function App() {
               document.getElementById('github-username-input').value !== null &&
               document.getElementById('github-username-input').value !== " "
             ) {
-              const githubUserData = fetchUser(document.getElementById('github-username-input').value);
-              
-              if (githubUserData) {
-                setWidget(true);
-              } else {
-                setWidget(false);
-              }
+                const githubUsernameInput = document.getElementById('github-username-input').value;
+                const githubUsernameResponse = fetchUser(githubUsernameInput);
+
+                if (githubUserDataRef !== null) {
+                  setWidget(true);
+                  setGithubUserData(githubUsernameResponse);
+                } else {
+                  setWidget(false);
+                }
             }
           }}
         >
           Create Widget
         </button>
+      </div>
+      <div className="github-widget-wrapper mt-8">
+        <GitHubWidget cardState={widgetState} widgetData={githubUserDataRef} />
       </div>
     </div>
   )
@@ -71,10 +77,22 @@ function fetchUser(username) {
     })
 }
 
-function GitHubWidget(widgetData, cardState) {
+function GitHubWidget({widgetData, cardState}) {
   if (cardState) {
-
+    return (
+      <React.Fragment>
+        <div className="active-card p-4 border-1 border-slate-500 rounded-md w-52 h-36">
+          <h3 className="github-username">@username</h3>
+        </div>
+      </React.Fragment>
+    )
   } else {
-    
+    return (
+      <React.Fragment>
+        <div className="disabled-card p-4 border-2 border-dashed text-slate-400 border-slate-300 rounded-md w-52 h-36 flex flex-row items-center justify-center">
+          <h3 className="font-semibold text-sm text-center">No Username</h3>
+        </div>
+      </React.Fragment>
+    )
   }
 }
